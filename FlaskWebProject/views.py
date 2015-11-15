7463884d424ec0b91a05cf7fdee621fa29ee3b1b
Jasmine Kim <jasminekim95@yahoo.com>
2015-11-14 21:05:26 -0800

    added request

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,57 +1,52 @@
 """
 Routes and views for the flask application.
 """
-from flask import Flask
-from flask_restful import Resource, Api
+
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 
+from flask import Flask, url_for, json, request
+
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/addresses', methods = ['GET', 'POST'])
+def api_address():
+    if request.method == 'POST':
+        print "HELLO"
+        # print request.mimetype
+        # print request.get_json()
+        # if request.headers['Content-Type'] == 'application/json':
+        return "JSON address: "  + json.dumps(request.json) + '\n'
+        # else:
+        #   return "Unsupported Media Type!"
 
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
 
-api.add_resource(HelloWorld, '/')
-
-# @app.route('/home')
-# def home():
-#     """Renders the home page."""
-#     return render_template(
-#         'index.html',
-#         title='Home Page',
-#         year=datetime.now().year,
-#     )
-
-# @app.route('/contact')
-# def contact():
-#     """Renders the contact page."""
-#     return render_template(
-#         'contact.html',
-#         title='Contact',
-#         year=datetime.now().year,
-#         message='Your contact page.'
-#     )
-
-# @app.route('/about')
-# def about():
-#     """Renders the about page."""
-#     return render_template(
-#         'about.html',
-#         title='About',
-#         year=datetime.now().year,
-#         message='Your application description page.'
-#     )
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)               # debug mode should be off when in production mode!
-
-
-
-    
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
--- api.py
+++ api.py
@@ -1,14 +1,67 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+#todoid = address
+#todo = COORDS
+
+def abort_if_coord_doesnt_exist(address):
+    if address not in ADDRESS:
+        abort(404, message="ADDRESS {} doesn't exist".format(address))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('coordinates', action='append')
+
+
+# For individual address
+# class Coords(Resource):
+    # def get(self, address):
+    #     abort_if_coord_doesnt_exist(address)
+    #     return COORDS[address]
+
+    # def delete(self, address):
+    #     abort_if_coord_doesnt_exist(address)
+    #     del COORDS[address]
+    #     return '', 204
+
+    # def put(self, address):
+    #     args = parser.parse_args()
+    #     task = {'task': args['task']}
+    #     COORDS[address] = task
+    #     return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class CoordsList(Resource):
+    def get(self):
+        return ADDRESS
+
+    def post(self):
+        args = parser.parse_args()
+        for urg in args:
+        	print "HELLOOOOOOOOOOOOO"
+        	print urg;
+        exit(0);
+        address = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(CoordsList, '/coords')
+# api.add_resource(Coords, '/coords/<address>')
+
 
 if __name__ == '__main__':
-    app.run()				# debug mode should be off when in production mode!
+    app.run(debug=True)
+
+
+
+
+
